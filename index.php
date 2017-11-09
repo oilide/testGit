bea814cbf23e705cf9fb0a3ccbb9c24c47c22fb2
oilide <oulcaid10500@aol.fr>
2017-11-09 09:53:49 +0100

    ikjgljkgklhj

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="css/bootstrap.css" rel="stylesheet">
+    <title>GIT</title>
+</head>
+
+<body>
+    <h1>On va tester GIT</h1>
+        <div class="container">
+            <div class="row">
+                <?php echo 'nous somme le ' . date('d-m-y'); ?>
+            </div>
+            <img src="le-voyage-arlo-promo-9.jpg">
+        </div>
+</body>
+</html>
